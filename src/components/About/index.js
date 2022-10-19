c3977a826db01bf3a416e2c02349528f8433fd5f
goforward-z <zlepeng.ucf@gmail.com>
2022-10-18 22:34:01 -0400

    update about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,15 +5,22 @@ export const About = () => {
         <div className="about">
             <h2>Nice to meet you.</h2>
             <strong>
-                I am an enthusiastic software developer with a passion for creating web
-                applications and
-                maintaining enhanced user experiences built on JavaScript technologies.
+            Software developer leveraging business management background to build a more intuitive user experience on the web. 
+            Recently earned a certificate in full stack development from the University of Central Florida,
+            with newly developed skills in JavaScript, CSS, React.js, and responsive web design. 
+            I am known as an innovative problem solver passionate about developing apps, focusing on mobile-first design and development. 
+            With each project, my aim is best to engage my audience for an impactful user experience. 
+            I applied aspects of UX and agile development in a recent project. 
+            I worked on a team of four to develop a single-page MERN app that is a light-weight social networking site aimed at changing the way one communicates through purchasing credits earned through signing up, 
+            likes received through posts made, and when directly purchasing credits.
+            I'm excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web. 
             </strong>
 
             <h2 className="mt-5">Skills</h2>
             <div className="content">
                 <div>
                     <strong>Languages</strong>
+                    <li>Java</li>
                     <li>JavaScript</li>
                     <li>HTML</li>
                     <li>CSS</li>
